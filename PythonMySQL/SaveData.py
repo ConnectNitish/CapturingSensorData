8f046d5d0593b5efc795d325c5fb93d89730da0d
ConnectNitish <nitish068@gmail.com>
2019-03-23 03:16:44 +0530

    Gathering Sensor Data Proejct

---
+++ PythonMySQL/SaveData.py
@@ -0,0 +1,177 @@
+import json
+from pprint import pprint
+import re
+import database as db
+import shutil
+import os
+import datetime
+
+
+battery_cols = ["userhash" ,"icon_small" ,"invalid_charger" ,"level" ,"health" ,"scale" ,"priority" ,"technology" , "charge_counter" ,"max_charging_current" ,"probe_transmit_mode" ,"voltage" ,"timestamp" ,"temperature" ,"max_charging_voltage" ,"present" ,"battery_low" ,"plugged" ,"seq" ,"status"]
+callstate_cols = ["userhash" ,"call_state" ,"timestamp" ,"probe_transmit_mode" ]
+datecalender_cols = ["userhash" ,"day_of_year" ,"month" ,"dst_offset" ,"day_of_month" ,"week_of_month" ,"probe_transmit_mode" ,"day_of_week_in_month"  ,"week_of_year"  ,"hour_of_day"  ,"timestamp"  ,"minute"  ,"day_of_week"]
+network_cols = ["userhash"  ,"interface_name"  , "interface_display"  , "hostname"  , "timestamp"  , "ip_address"  ,"probe_transmit_mode"]
+wakelockinformation_cols = ["userhash"  ,"bright_count"  ,"dim_locks"  , "partial_count"  ,"timestamp"  , "full_locks"  , "bright_locks"  , "partial_locks"  , "dim_count"  , "probe_transmit_mode"   , "full_count"]
+wifiaccesspoints_cols = ["userhash"  ,"current_rssi"  , "timestamp"  , "current_bssid"  ,"access_point_count"  ,"probe_transmit_mode"  ,"current_link_speed"  , "current_ssid" ]
+sunrisesunsetfeature_cols = ["userhash"  ,"sunset"  , "timestamp"  , "longitude"  ,"sunrise"  ,"is_day"  ,"sunrise_distance"  ,"sunset_distance"  ,"probe_transmit_mode"  , "latitude"  ,"day_duration" ]
+location_cols = ["userhash"  ,"time_fix"  , "longitude"  ,"altitude"  ,"probe_transmit_mode"  , "accuracy"  , "gps_available"  ,"speed"  ,"timestamp"  , "provider"  ,"network_available"  ,"bearing"  ,"latitude"]
+def isColPresent(tname,key):
+    key = key.lower()
+    if(tname.find("BatteryProbe")!=-1):
+        if(key in battery_cols):
+            return True
+
+    if(tname.find("CallStateProbe")!=-1):
+        if(key in callstate_cols):
+            return True
+    if(tname=="DateCalendarProbe"):
+        if(key in datecalender_cols):
+            return True
+    if(tname=="NetworkProbe"):
+        if(key in network_cols):
+            return True
+    if(tname=="WakeLockInformationProbe"):
+        if(key in wakelockinformation_cols):
+            return True
+    if(tname=="WifiAccessPointsProbe"):
+        if(key in wifiaccesspoints_cols):
+            return True
+    if(tname=="SunriseSunsetFeature"):
+        if(key in sunrisesunsetfeature_cols):
+            return True
+    if(tname=="LocationProbe"):
+        if(key in location_cols):
+            return True
+    
+    return False
+
+def timeStamped(fname, fmt='%Y-%m-%d-%H-%M-%S_{fname}'):
+    return datetime.datetime.now().strftime(fmt).format(fname=fname)
+
+def writeLog(message):
+	log_file="log_file"
+	exists = os.path.isfile(log_file)	
+	if not exists:
+		handler = open(log_file,'w')
+		handler.write(timeStamped(""))
+		handler.write(message)
+		handler.write("\n")
+
+def InvokeDBFunc():
+	newobj = db.Database("127.0.0.1",'python_mysql','postgres','test')
+	x = newobj.probe_info()
+	print(x)
+	newobj.close_conn_cursor()
+	return x
+
+class SaveData:
+
+	def SaveDataToDB(self):
+		try:
+
+			#newobj = db.Database("127.0.0.1",'mydb3','dbuser','qwerty')
+			newobj = db.Database("127.0.0.1",'python_mysql','postgres','test')
+			# newobj.printRows("schema1.table1")
+			getCurrentTimeStamp = timeStamped("")
+			name_of_file_to_Store="Input_feed.json"
+
+			#with open('My_Phone_Data.json') as f:
+			#with open('2019-03-21-13-29-54_data_By_app_2.json') as f:
+			with open(name_of_file_to_Store) as f:
+			    data = json.load(f)
+
+			# pprint(data)
+			userdata = data["UserHash"]
+			# print(userdata)
+			payload = data["Payload"]
+			# print(type(payload))
+
+			payload_json = json.loads(payload)
+			# pprint(payload_json)
+
+			payload_dict = {}
+
+			for item in payload_json:
+
+				# print(item)
+				# print("####################################################################")
+
+				probe_dict = {}
+
+				tname = re.split('[.]',item["PROBE"])[-1]
+				# print("tname is ----",tname)
+				# print(re.split('[.]',item["PROBE"]))
+
+				if(tname!="SoftwareInformationProbe"):
+					payload_dict[tname] = []
+				record = []
+				record.append(userdata)
+				
+				for key,values in item.items():
+					if key != "PROBE":
+						if key!= "GUID" and key!="ACCESS_POINTS":
+							record.append(str(values))
+						if(tname!="SoftwareInformationProbe"):
+							payload_dict[tname].append((key,values))
+
+			# pprint(payload_dict)
+
+			for k,v in payload_dict.items():
+				col_str = ""
+				record_list = []
+				s_str = ""
+
+				col_str+="userhash"
+				record_list.append(str(userdata).lower())
+				col_str+=","
+				s_str+="%s,"
+				print(k)
+				for myval in v:
+					if(isColPresent(k,myval[0])):
+						col_str+=str(myval[0]).lower()
+						record_list.append(str(myval[1]).lower())
+						col_str+=","
+						s_str+="%s,"
+				col_str = col_str[:len(col_str)-1]
+				s_str = s_str[:len(s_str)-1]
+				# record_str = record_str[:len(record_str)-1]
+				print("col_str ::",col_str)
+				print(s_str)
+				print(record_list)
+				print("-----------------IN LAYER HI--------------------------")
+				newobj.insert(k,col_str,s_str,tuple(record_list),name_of_file_to_Store)
+				print("-----------------IN LAYER HI 2--------------------------")
+
+			newobj.close_conn_cursor()
+
+			#move the file to back up Folder 
+			destination_directory = "./BackUp/"+getCurrentTimeStamp
+			if not os.path.exists(destination_directory):
+				os.makedirs(destination_directory)
+			print("GOOD---------------------")
+			shutil.move(name_of_file_to_Store, destination_directory)
+			
+		except:
+			#Some Error Occurered 
+			print("-------------------------------EX------------------------------------------")
+			writeLog("--- Parsing File Problem ---- "+getCurrentTimeStamp)
+			#move the file to back up Folder 
+			destination_directory = "./Error/"+getCurrentTimeStamp
+			if not os.path.exists(destination_directory):
+			    os.makedirs(destination_directory)
+			shutil.move(name_of_file_to_Store, destination_directory)
+			newobj.close_conn_cursor()			
+
+			#pprint(payload_dict)
+
+		# d = payload_dict['WakeLockInformationProbe']
+
+		# for item in d:
+
+		# 	print(item)
+
+if __name__ == "__main__":
+	SaveDataobj = SaveData()
+	SaveDataobj.SaveDataToDB()
+	# InvokeDBFunc()
+	
---
+++ PythonMySQL/database.py
@@ -0,0 +1,197 @@
+import mysql.connector
+from mysql.connector import Error
+import psycopg2
+from datetime import datetime
+
+class Database:
+    my_cursor = None
+    conn = None
+    def __init__(self,host,dbname,user,pwd):
+        self.host = host
+        self.dbname = dbname
+        self.user  = user
+        self.password = pwd
+        self.conn = psycopg2.connect(host=host,
+                                    database=dbname,
+                                    user=user,
+                                    password=pwd)
+        self.my_cursor =  self.conn.cursor()
+        print("---------------Object Creation IN DB ------------------------------")
+
+    def probe_info(self):
+        ret_dict = {}
+        x1 = self.get_battery()
+        uids = {}
+        for r in x1:
+            dt_object = datetime.fromtimestamp(int(r[2]))
+	    dt_object = r[2]
+            uids[r[0]] = [r[1],str(dt_object)]
+            # print(r[0])
+        ret_dict["battery"]=uids
+        # print(uids)
+        # print("----------------------------")
+        x2 = self.get_callState()
+        uids = {}
+        for r in x2:
+            dt_object = datetime.fromtimestamp(int(r[2]))
+            uids[r[0]] = [r[1],str(dt_object)]
+            # print(r[0])
+        ret_dict["callstate"]=uids
+        # print(uids)
+        # print("----------------------------")
+        x3 = self.get_location()
+        uids = {}
+        for r in x3:
+            dt_object = datetime.fromtimestamp(int(r[3]))
+            uids[r[0]] = [r[1],r[2],str(dt_object)]
+            # print(r[0])
+        ret_dict["location"]=uids
+        # print(uids)
+        return ret_dict
+
+    
+    def get_battery(self):
+        self.my_cursor.execute("""select userhash,level,timestamp from battery order by timestamp""")
+        rows = self.my_cursor.fetchall()
+        # print(rows)
+        return rows
+    
+    def get_callState(self):
+        self.my_cursor.execute("""select userhash,call_state,timestamp from callstate order by timestamp""")
+        rows = self.my_cursor.fetchall()
+        # print(rows)
+        return rows
+
+    def get_location(self):
+        self.my_cursor.execute("""select userhash,longitude,latitude,timestamp from location order by timestamp""")
+        rows = self.my_cursor.fetchall()
+        # print(rows)
+        return rows
+
+    def printRows(self,tname):
+        if(tname == "CallStateProbe"):
+            self.my_cursor.execute("""select * from callstate""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "DateCalendarProbe"):
+            self.my_cursor.execute("""select * from datecalendar""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "NetworkProbe"):
+            self.my_cursor.execute("""select * from network""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "WakeLockInformationProbe"):
+            self.my_cursor.execute("""select * from wakelockinformation""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "BatteryProbe"):
+            self.my_cursor.execute("""select * from battery""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "WifiAccessPointsProbe"):
+            self.my_cursor.execute("""select * from wifiaccesspoints""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "SunriseSunsetFeature"):
+            self.my_cursor.execute("""select * from sunrisesunsetfeature""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+        elif(tname == "LocationProbe"):
+            self.my_cursor.execute("""select * from location""")
+            rows = self.my_cursor.fetchall()
+            print(rows)
+
+    def createInsert(self,tname1,columnlist,columnlistpercents):
+        sql_insert_query = " INSERT INTO " + tname1 + "(" + columnlist + ")" +   " VALUES ( " + columnlistpercents +")"
+        print(sql_insert_query)
+        return sql_insert_query
+
+    # newobj.insert(k,col_str,s_str,tuple(record_list),myf)
+
+    def insert(self,tname,col_str,s_str,record_list,myf):
+        sql_insert_query = ""
+        print("tname ----- IN DB ",tname)
+        if(tname == "CallStateProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            sql_insert_query = self.createInsert('callstate',col_str,s_str)
+            # sql_insert_query = """ INSERT INTO callstate (userhash,call_state ,timestamp ,probe_transmit_mode) 
+            #                VALUES (%s,%s,%s,%s) """
+
+        elif(tname == "DateCalendarProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO datecalendar (userhash,day_of_year,month,dst_offset,day_of_month,week_of_month,probe_transmit_mode,day_of_week_in_month,week_of_year,hour_of_day,timestamp,minute ,day_of_week) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('datecalendar',col_str,s_str)
+
+
+        elif(tname == "NetworkProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO network (userhash,interface_name , interface_display , hostname , timestamp , ip_address ,probe_transmit_mode) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('network',col_str,s_str)
+
+        elif(tname == "WakeLockInformationProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO wakelockinformation (userhash,bright_count ,dim_locks, partial_count ,timestamp, full_locks, bright_locks, partial_locks, dim_count, probe_transmit_mode , full_count) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('wakelockinformation',col_str,s_str)
+
+        elif(tname == "BatteryProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO battery (userhash,icon_small ,invalid_charger ,level ,health ,scale ,priority ,technology , charge_counter ,max_charging_current ,probe_transmit_mode ,voltage ,timestamp ,temperature ,max_charging_voltage ,present ,battery_low ,plugged ,seq ,status ) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('battery',col_str,s_str)
+
+        elif(tname == "WifiAccessPointsProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO wifiaccesspoints (userhash,current_rssi , timestamp , current_bssid ,access_point_count ,probe_transmit_mode ,current_link_speed , current_ssid  ) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('wifiaccesspoints',col_str,s_str)
+
+        elif(tname == "SunriseSunsetFeature"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO sunrisesunsetfeature (userhash,sunset , timestamp , longitude ,sunrise ,is_day ,sunrise_distance ,sunset_distance,probe_transmit_mode , latitude ,day_duration  ) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('sunrisesunsetfeature',col_str,s_str)
+
+
+        elif(tname == "LocationProbe"):
+            print("values to insert-----------------------")
+            print(record_list)
+            # print("=====================================")
+            # sql_insert_query = """ INSERT INTO location (userhash ,time_fix , longitude ,altitude ,probe_transmit_mode , accuracy , gps_available ,speed ,timestamp , provider ,network_available ,bearing ,latitude  ) 
+            #                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            sql_insert_query = self.createInsert('location',col_str,s_str)
+
+        print()
+
+        if(sql_insert_query!=""):
+            try:
+                result = self.my_cursor.execute(sql_insert_query, record_list)
+                print(result)
+            except:
+                print("failed insert for table ",tname," for file ",myf)
+            # self.my_cursor.execute("""insert into """+tname+""" (id,password) values("""+str(a)+""","""+b+""")""" )
+            self.conn.commit()
+        else:
+            print("new probe found with tname------",tname)
+
+    def close_conn_cursor(self):
+        self.my_cursor.close()
+        self.conn.close()
+
